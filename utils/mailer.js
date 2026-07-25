8409937ac6da8c3f60c34c17e99f937450e758fd
Sky121122 <saket121122@gmail.com>
2026-07-25 08:06:50 +0530

    bravo email service

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,25 +1,13 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
-    tls: {
-        rejectUnauthorized: false,
-    },
-});
-
-transporter.verify((error, success) => {
-    if (error) {
-        console.log("SMTP ERROR:");
-        console.log(error);
-    } else {
-        console.log("SMTP SERVER READY");
-    }
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
 });
 
 export default transporter;
