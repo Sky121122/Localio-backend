8019fc7dc36f335db4a0677c1d94321290994fd9
Sky121122 <saket121122@gmail.com>
2026-07-27 19:55:58 +0530

    new modal schema added in user

--- models/User.js
+++ models/User.js
@@ -41,6 +41,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     default: "",
 },
+favorites: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Business"
+}],
 },
     {
         timestamps: true,
